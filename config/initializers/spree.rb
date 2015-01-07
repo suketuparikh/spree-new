72d7a7423acf8b9ab7b3b1835952182a87e90a8a
Suketu Parikh <suketuparikh@gmail.com>
2015-01-07 16:07:17 +0530

    updated

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,10 +9,39 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
-  config.use_s3 = true
-  config.s3_bucket = 'ccaccspreeimages'
-  config.s3_access_key = "AKIAID7BB5XBLZFDYJRA"
-  config.s3_secret = "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"
+  #config.use_s3 = true
+  #config.s3_bucket = 'ccaccspreeimages'
+  #config.s3_access_key = "AKIAID7BB5XBLZFDYJRA"
+  #config.s3_secret = "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"
+  attachment_config = {
+
+	  s3_credentials: {
+		access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+		secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+		bucket:            ENV['S3_BUCKET_NAME']
+	  },
+
+	  storage:        :s3,
+	  s3_headers:     { "Cache-Control" => "max-age=31557600" },
+	  s3_protocol:    "https",
+	  bucket:         ENV['S3_BUCKET_NAME'],
+	  url:            ":s3_domain_url",
+
+	  styles: {
+		  mini:     "48x48>",
+		  small:    "100x100>",
+		  product:  "240x240>",	
+		  large:    "600x600>"
+	  },
+
+	  path:           "/spree/:class/:id/:style/:basename.:extension",
+	  default_url:    "/spree/:class/:id/:style/:basename.:extension",
+	  default_style:  "product"
+	}
+
+	attachment_config.each do |key, value|
+	  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+	end
 end
 
 Spree.user_class = "Spree::User"
