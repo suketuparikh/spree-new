807f1f750aef2526143be4fc2eedcbe137a89066
Suketu Parikh <suketuparikh@gmail.com>
2015-01-12 16:47:51 +0530

    updated

--- config/initializers/wombat.rb
+++ config/initializers/wombat.rb
@@ -3,12 +3,12 @@ Spree::Wombat::Config.configure do |config|
   config.connection_token = ENV['WOMBAT_TOKEN']
   config.connection_id = ENV['WOMBAT_CONNECTION_ID']
 
-  config.push_objects = ["Spree::Order", "Spree::Product", "Spree::Customer"]
+  config.push_objects = ["Spree::Order", "Spree::Product"]
   config.payload_builder = {
   # By default we filter orders to only push if they are completed.  You can remove the filter to send incomplete orders as well.
 	"Spree::Order" => { serializer: "Spree::Wombat::OrderSerializer", root: "orders", filter: "complete" },
     "Spree::Product" => { serializer: "Spree::Wombat::ProductSerializer", root: "products" },
-    "Spree::Customer" => { serializer: "Spree::Wombat::CustomerSerializer", root: "customers" }
+    #"Spree::Customers" => { serializer: "Spree::Wombat::CustomerSerializer", root: "customers" }
   }
   #config.push_url = "https://push.wombat.co"
 
