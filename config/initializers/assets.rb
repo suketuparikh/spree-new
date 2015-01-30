13f6775893811c588570d5cb6e5ddd0328cfc8b4
Suketu Parikh <suketuparikh@gmail.com>
2015-01-30 20:47:52 +0530

    updated theme css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( bx_loader.gif )
 #Rails.application.config.assets.precompile += %w (spree_admin.css)
+Rails.application.config.assets.precompile += %w( logo/spree_50.png )
+Rails.application.config.assets.precompile += %w( spree/fancy/logo-white.png )
+Rails.application.config.assets.precompile += %w( spree/fancy/fancy.css )
