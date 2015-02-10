1bc68b4ab2ab3574191d4132f1c823ef1e3a1eaa
Suketu Parikh <suketuparikh@gmail.com>
2015-02-10 20:10:03 +0530

    updated

--- config/initializers/wombat.rb
+++ config/initializers/wombat.rb
@@ -10,7 +10,7 @@ Spree::Wombat::Config.configure do |config|
 	#	"Spree::Product" => { serializer: "Spree::Wombat::ProductSerializer", root: "products" },
   #   "Spree::StockItem" => { serializer: "Spree::Wombat::StockItemSerializer", root: "inventories" }
   }
-  # config.push_url = "http://developer.dendroidal.com/spree/"
-  config.push_url = "http://54.149.177.150/SalesforceService.asmx?op=PushDataInSalesforceWithoutEmail"
+  config.push_url = "http://developer.dendroidal.com/spree/"
+  #config.push_url = "http://54.149.177.150/SalesforceService.asmx?op=PushDataInSalesforceWithoutEmail"
 
 end
