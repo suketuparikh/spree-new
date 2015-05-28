cdacec357f8b8de3470f3b67d5a212af09e3fae1
Suketu Parikh <suketuparikh@gmail.com>
2015-05-28 15:01:42 +0530

    updated

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -10,7 +10,7 @@ Spree.config do |config|
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
   
-  config.s3_host_alias = "s3-us-west-2.amazonaws.com",
+  config.s3_host_alias = "s3-us-west-2.amazonaws.com"
  
   attachment_config = {
 
