38b215dab79bd174155a973653f79eefd751ba8f
Suketu Parikh <suketuparikh@gmail.com>
2015-05-28 17:46:51 +0530

    update

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -10,40 +10,40 @@ Spree.config do |config|
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
  
-#  attachment_config = {
-#
-#	  s3_credentials: {
-#		access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-#		secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-#		bucket:            ENV['S3_BUCKET_NAME']
-#	  },
-#
-#	  storage:        :s3,
-#	  s3_headers:     { "Cache-Control" => "max-age=31557600" },
-#	  s3_protocol:    "https",
-#	  bucket:         ENV['S3_BUCKET_NAME'],
-#	  url:            ":s3_domain_url",
-#
-#	  styles: {
-#		  mini:     "48x48>",
-#		  small:    "100x100>",
-#		  product:  "240x240>",	
-#
-#	  },
-#
-#	  path:           ":rails_root/public/:class/:attachment/:id/:style/:basename.:extension",
-#	  default_url:    "/:class/:attachment/:id/:style/:basename.:extension",
-#	  default_style:  "product"
-#	}
-#
-#	attachment_config.each do |key, value|
-#	  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-#	end
+  attachment_config = {
+
+	  s3_credentials: {
+		access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+		secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+		bucket:            ENV['S3_BUCKET_NAME']
+	  },
+
+	  storage:        :s3,
+	  s3_headers:     { "Cache-Control" => "max-age=31557600" },
+	  s3_protocol:    "https",
+	  bucket:         ENV['S3_BUCKET_NAME'],
+	  url:            ":s3_domain_url",
+
+	  styles: {
+		  mini:     "48x48>",
+		  small:    "100x100>",
+		  product:  "240x240>",	
+
+	  },
+
+	  path:           ":rails_root/public/:class/:attachment/:id/:style/:basename.:extension",
+	  default_url:    "/:class/:attachment/:id/:style/:basename.:extension",
+	  default_style:  "product"
+	}
+
+	attachment_config.each do |key, value|
+	  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+	end
 
 end
 
 Spree.user_class = "Spree::User"
 
-#Paperclip.interpolates(:s3_domain_url) do |attachment, style|
-#"#{attachment.s3_protocol}://#{attachment.s3_host_name}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
-#end
+Paperclip.interpolates(:s3_domain_url) do |attachment, style|
+"#{attachment.s3_protocol}://#{attachment.s3_host_name}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
+end
