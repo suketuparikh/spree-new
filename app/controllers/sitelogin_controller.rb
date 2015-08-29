a0cd2eb2ad912649132ea8e70fc3a53f7aa052ca
Suketu Parikh <suketuparikh@gmail.com>
2015-08-29 23:16:32 +0530

    updated

--- app/controllers/sitelogin_controller.rb
+++ app/controllers/sitelogin_controller.rb
@@ -6,7 +6,7 @@ class SiteloginController < ApplicationController
   def login
     session[:valid_sitelogin] = false;
     
-    if params['site_password'] == "POWERBEATNOW"
+    if params['site_password'].upcase == "POWERBEATNOW"
       session[:valid_sitelogin] = true;
       redirect_to spree.products_path
     else
