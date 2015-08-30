b7f6f7df0e8421abe4bc7bb49be7bb33dd7d52a3
Suketu Parikh <suketuparikh@gmail.com>
2015-08-30 23:28:35 +0530

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #http_basic_authenticate_with name: "POWERBEATNOW", password: "POWERBEATNOW"
+
+  def index
+  	redirect_to ('/products/pbprometer1')
+  end
 end
