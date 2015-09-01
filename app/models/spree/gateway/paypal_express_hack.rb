b0299f2eff20871cb7d8179e8f89531e58ee39e2
Suketu Parikh <suketuparikh@gmail.com>
2015-09-02 00:19:21 +0530

    updated

--- app/models/spree/gateway/paypal_express_hack.rb
+++ app/models/spree/gateway/paypal_express_hack.rb
@@ -2,6 +2,6 @@ Spree::Gateway::PayPalExpress.class_eval do
   def credit(credit_cents, response_code, gateway_options)
     # TODO: this should use the `refund` method, but it needs some work.
     # Doing nothing for now to prevent an error and allow orders to be cancelled.
-    OpenStruct.new(success?:true,warning:'PayPal credit method not implemented, make sure payments are cancelled manually')
+    warning:'PayPal credit method not implemented, make sure payments are cancelled manually'
   end
 end
