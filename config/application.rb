82e640304268e302d80ceef23d283a48b75c37cb
rakesh <rbaldha@nishutatechnolabs.com>
2016-01-22 16:12:33 +0530

    modified file

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ Bundler.require(*Rails.groups)
 module Newstore
   class Application < Rails::Application
     config.cache_store = :dalli_store
-    config.serve_static_assets = false
-	config.assets.initialize_on_precompile = false
-config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.serve_static_assets = true
+	config.assets.initialize_on_precompile = true
+
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
