a96f0e44c1f14d07396ee161356f37adec1cdc03
rakesh <rbaldha@nishutatechnolabs.com>
2016-02-15 15:49:30 +0530

    changes updated2

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -6,12 +6,13 @@ Spree::Order.class_eval do
 
   def add_user_guest 
   	
-   @test_password = SecureRandom.hex(8)
-   @user = Spree::User.new(:email => self.email, :password => @test_password, :password_confirmation => @test_password)
-   @user.spree_roles << Spree::Role.find_or_create_by(name: "user")
+  #editing Developer remove hash sign on code then create new user
+  # @test_password = SecureRandom.hex(8)
+  # @user = Spree::User.new(:email => self.email, :password => @test_password, :password_confirmation => @test_password)
+  # @user.spree_roles << Spree::Role.find_or_create_by(name: "user")
 
-@user.save
-Spree::BaseMailer::mail(to:self.email, from:'jainesh@commerceacceleration.com', subject:'Watteam Your New password', :body => @test_password).deliver
+#@user.save
+#Spree::BaseMailer::mail(to:self.email, from:'jainesh@commerceacceleration.com', subject:'Watteam Your New password', :body => @test_password).deliver
 
 
   
