fbcb1f427a6122bde4df0cfcdb987f597800d979
rakesh <rbaldha@nishutatechnolabs.com>
2016-02-15 17:53:08 +0530

    update

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-# Be sure to restart your server when you modify this file.
-
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( bx_loader.gif )
-Rails.application.config.assets.precompile += %w( favicon.ico )
-#Rails.application.config.assets.precompile += %w (spree_admin.css)
-Rails.application.config.assets.precompile += %w( logo/spree_50.png )
-Rails.application.config.assets.precompile += %w( spree_50.png )
-Rails.application.config.assets.precompile += %w( spree/fancy/logo-white.png )
-#Rails.application.config.assets.precompile += %w( spree/fancy/fancy.css )
-Rails.application.config.assets.precompile += %w( icons.svg icons.ttf icons.eot icons.woff )
-Rails.application.config.assets.precompile += %w( images/bx_loader.gif images/controls.png )
-Rails.application.config.assets.precompile += %w( FACEBOOK.png )
-Rails.application.config.assets.precompile += %w( LINKED.png )
-Rails.application.config.assets.precompile += %w( TW.png )
-Rails.application.config.assets.precompile += %w( YU.png )
-Rails.application.config.assets.precompile += %w( phone-contact.png )
-Rails.application.config.assets.precompile += %w( bullets.png )
-Rails.application.config.assets.precompile += %w( powerbeat_logo.png )
-Rails.application.config.assets.precompile += %w( down_menu_white.png )
-Rails.application.config.assets.precompile += %w( down_menu_black.png )
-Rails.application.config.assets.precompile += %w( bullets.png )
-Rails.application.config.assets.precompile += %w( powerbeat-logo-blue.png )
-Rails.application.config.assets.precompile += %w( toted.png )
-Rails.application.config.assets.precompile += %w( cart.png )
-Rails.application.config.assets.precompile += %w( pipe.png )
-Rails.application.config.assets.precompile += %w( mid_banner.jpg )
-Rails.application.config.assets.precompile += %w( popup_bg.png )
-Rails.application.config.assets.precompile += %w( popup_main_bg.jpg )
-Rails.application.config.assets.precompile += %w( doted.png )
-Rails.application.config.assets.precompile += %w( menu.png )
-
-Rails.application.config.assets.precompile += %w( f_facebook.png )
-Rails.application.config.assets.precompile += %w( f_linkedin.png )
-Rails.application.config.assets.precompile += %w( f_twitter.png )
-Rails.application.config.assets.precompile += %w( f_youtube.png )
-Rails.application.config.assets.precompile += %w( foot-nav-devider.jpg )
-
-Rails.application.config.assets.precompile += %w( fancybox_overlay.png )
-Rails.application.config.assets.precompile += %w( fancybox_sprite.png )
-Rails.application.config.assets.precompile += %w( fancybox_sprite@2x.png )
-Rails.application.config.assets.precompile += %w( fancybox_loading@2x.gif )
-
-
-Rails.application.config.assets.precompile += %w( spree/frontend/fonts/flexslider-icon.eot spree/frontend/fonts/flexslider-icon.svg spree/frontend/fonts/flexslider-icon.ttf spree/frontend/fonts/flexslider-icon.woff )
-=======
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
@@ -126,4 +74,4 @@ Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf .woff2)
 
 
 Rails.application.config.assets.precompile += %w( spree/frontend/fonts/flexslider-icon.eot spree/frontend/fonts/flexslider-icon.svg spree/frontend/fonts/flexslider-icon.ttf spree/frontend/fonts/flexslider-icon.woff )
->>>>>>> staging
+
--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-# Configure Spree Preferences
-#
-# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
-#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
-#
-# In order to initialize a setting do:
-# config.setting_name = 'new value'
-Spree.config do |config|
-  # Example:
-  # Uncomment to stop tracking inventory levels in the application
-  # config.track_inventory_levels = false
- 
-  attachment_config = {
-
-	  s3_credentials: {
-		access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-		secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-		bucket:            ENV['S3_BUCKET_NAME']
-	  },
-
-	  storage:        :s3,
-	  s3_headers:     { "Cache-Control" => "max-age=31557600" },
-	  s3_protocol:    "https",
-	  bucket:         ENV['S3_BUCKET_NAME'],
-	  url:            ":s3_domain_url",
-
-	  styles: {
-		  mini:     "48x48>",
-		  small:    "100x100>",
-		  product:  "340x340>"
-	  },
-
-	  path:           ":rails_root/public/:class/:attachment/:id/:style/:basename.:extension",
-	  default_url:    "/:class/:attachment/:id/:style/:basename.:extension",
-	  default_style:  "product"
-	}
-
-	attachment_config.each do |key, value|
-	  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-	end
-
-end
-
-Spree.user_class = "Spree::User"
-
-Paperclip.interpolates(:s3_domain_url) do |attachment, style|
-"#{attachment.s3_protocol}://#{attachment.s3_host_name}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
-end
-
-Spree::Config[:always_include_confirm_step] = true
-Spree::Auth::Config[:registration_step] = true
-Spree::Config[:checkout_zone] = "North America"
-=======
 # Configure Spree Preferences
 #
 # Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
@@ -104,4 +50,3 @@ end
 Spree::Config[:always_include_confirm_step] = true
 Spree::Auth::Config[:registration_step] = true
 Spree::Config[:checkout_zone] = "North America"
->>>>>>> staging
